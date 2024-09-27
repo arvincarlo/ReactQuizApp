51b389694aa276881bd3951b7c8e64e37f322db7
Arvin <arvincris@gmail.com>
2024-09-27 11:37:44 +0800

    Displaying Progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -57,7 +58,8 @@ export default function App() {
 
   const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
-
+  const maxPossiblePoints = questions.reduce((accumulator, question) => accumulator + question.points, 0);
+  
   useEffect(() => {
     fetch('http://localhost:8000/questions')
       .then(response => response.json())
@@ -73,6 +75,7 @@ export default function App() {
       {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
       {status === 'active' && (
         <>
+          <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}></Progress>
           <Question question={questions[index]} dispatch={dispatch} answer={answer} points={points}></Question>
           <NextButton dispatch={dispatch} answer={answer}></NextButton>
         </>
